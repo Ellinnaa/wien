2f77cb1c667eb0cc6ddf0ffe15dbd877f38bf01b
Ellinnaa <elenawiedmann@googleail.com>
2025-04-03 18:23:35 +0200

    basemaps einbinden

--- main.js
+++ main.js
@@ -21,10 +21,15 @@ let overlays={
 
 //Layercontrol
 L.control.layers({
-"BasemapAT grau" : L.tileLayer('https://mapsneu.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png', {
-    maxZoom: 19,
-    attribution: 'Hintergrundkarte: <a href="https://www.basemap.at">basemap.at</a>'
-}).addTo(map)
+"BasemapAT " : L.tileLayer.provider('BasemapAT.basemap').addTo(map),
+"BasemapAT grau" : L.tileLayer.provider('BasemapAT.grau'),
+"BasemapAT Overlay" : L.tileLayer.provider('BasemapAT.overlay'),
+"BasemapAT terrain" : L.tileLayer.provider('BasemapAT.terrain'),
+"BasemapAT surface" : L.tileLayer.provider('BasemapAT.surface'),
+"BasemapAT highdpi" : L.tileLayer.provider('BasemapAT.highdpi'),
+"BasemapAT orthofoto" : L.tileLayer.provider('BasemapAT.orthofoto'),
+"BasemapAT standaard" : L.tileLayer.provider('nlmaps.standaard'),
+
 }, {
     "Sehenswürdigkeiten": overlays.sights,
     "Viena sightseeing Linien": overlays.lines,
